298f1e22f07c7f3c2428b6878182d5068fe3ba79
SamMWBlake <sam.k.blake@gmail.com>
2013-09-10 13:31:02 -0500

    Added transitive commands and synonyms

--- lib/parser.rb
+++ lib/parser.rb
@@ -2,19 +2,35 @@ require 'parslet'
 
 class GhostParser < Parslet::Parser
   # Whitespace
-  rule(:space)      { match('\s').repeat(1) }
+  rule(:space)      { match(' ').repeat(1) }
+  rule(:space?)     { space.maybe }
   rule(:newline)    { match('\n') }
   rule(:blank_line) { newline.repeat(2) }
 
-  rule(:prompt)      { blank_line >> str('>') >> space }
-  rule(:label)       { (newline.absent? >> any).repeat(1) }
-  rule(:description) { (prompt.absent? >> any).repeat(1) }
-  rule(:command)     { str('go').absent? >> label }
-  rule(:action)      { (prompt >> command.as(:command)) >> blank_line >> description.as(:result) }
-  
+  # Prompts
+  rule(:prompt)         { str('>') >> space }
+  rule(:new_prompt)     { blank_line >> prompt }
+  rule(:synonym_prompt) { newline >> prompt }
+
+  # Text
+  rule(:wildcard)    { space >> str('*') >> space? }
+  rule(:label)       { (newline.absent? >> wildcard.absent? >> any).repeat(1) }
+  rule(:description) { (new_prompt.absent? >> any).repeat(1) }
+
+  # Commands
+  rule(:room_command_prefix)  { str('go') >> space }
+  rule(:room_command)         { room_command_prefix >> label.as(:room_name) }
+  rule(:transitive_command)   { room_command_prefix.absent? >> label.as(:transitive_command) >> wildcard }
+  rule(:intransitive_command) { room_command_prefix.absent? >> label.as(:command) }
+  rule(:command)              { transitive_command | intransitive_command }
+  rule(:commands)             { command >> (synonym_prompt >> command).repeat }
+
+  # Actions (commands + results)
+  rule(:action) { new_prompt >> commands.as(:commands) >> blank_line >> description.as(:result) }
+
   # Rooms
   rule(:exit) { newline >> str('  -> ') >> label.as(:exit)}
-  rule(:room) { prompt >> str('go') >> space >> label.as(:room_name) >> exit.repeat.as(:exits) >> blank_line >> description.as(:description) >> action.repeat.as(:local_actions) }
+  rule(:room) { new_prompt >> room_command >> exit.repeat.as(:exits) >> blank_line >> description.as(:description) >> action.repeat.as(:local_actions) }
 
   # Game
   rule(:game) { description.as(:start) >> action.repeat.as(:global_actions) >> room.repeat(1).as(:rooms) }
