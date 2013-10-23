3191fba283ff14ab8d347c1882a7f386f23773d3
SamMWBlake <sam.k.blake@gmail.com>
2013-10-23 15:03:54 -0700

    Added Markdown parsing

--- ajax/server.rb
+++ ajax/server.rb
@@ -1,3 +1,4 @@
+require 'kramdown'
 require 'sinatra'
 require_relative File.join "..", "test", "game"
 
@@ -13,9 +14,14 @@ end
 
 get '/start/' do
   session[:game] = test_game
-  session[:game].start
+  process_markup session[:game].start
 end
 
 get '/execute/*' do |command|
-  session[:game].execute command
+  process_markup session[:game].execute command
+end
+
+def process_markup(markup)
+  # Parse Markdown after accounting for alternate blockquote syntax
+  markdown markup.gsub /^    /, "> "
 end
